a41968129daf0aeb4afea1499b5e3f2a5eb75adc
leonardoigor <igor_mendonca@outlookj.com.br>
2020-08-02 16:56:30 -0300

    Updates

---
+++ precache-manifest.72ac85e974f770271c19ebd2aed50a11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ff0fd3659119a752e9af08fecb21d9",
+    "url": "/whatsApp-Clone-UI-build/index.html"
+  },
+  {
+    "revision": "dbafbd3bc4c1c22a0746",
+    "url": "/whatsApp-Clone-UI-build/static/css/main.cff23367.chunk.css"
+  },
+  {
+    "revision": "03e51133df4407e55a93",
+    "url": "/whatsApp-Clone-UI-build/static/js/2.a506b6dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/whatsApp-Clone-UI-build/static/js/2.a506b6dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbafbd3bc4c1c22a0746",
+    "url": "/whatsApp-Clone-UI-build/static/js/main.2fb70a69.chunk.js"
+  },
+  {
+    "revision": "309949befbbe15e524f9",
+    "url": "/whatsApp-Clone-UI-build/static/js/runtime-main.5a5a96ee.js"
+  },
+  {
+    "revision": "23e700d1c303ae465fc16d9c6e0690b0",
+    "url": "/whatsApp-Clone-UI-build/static/media/01.23e700d1.jpg"
+  },
+  {
+    "revision": "64e2c6e6b7387c2551c6e3f255711723",
+    "url": "/whatsApp-Clone-UI-build/static/media/02.64e2c6e6.jpg"
+  },
+  {
+    "revision": "c98cc75f2aa905314d74375a975d2cf2",
+    "url": "/whatsApp-Clone-UI-build/static/media/intro-connection-light.c98cc75f.jpg"
+  },
+  {
+    "revision": "f88400bd40cd5587da3a56b6c7b3402a",
+    "url": "/whatsApp-Clone-UI-build/static/media/ppI.f88400bd.jpeg"
+  },
+  {
+    "revision": "12a54701604fdf7016f790e438c9d4ac",
+    "url": "/whatsApp-Clone-UI-build/static/media/ppM.12a54701.jpeg"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/whatsApp-Clone-UI-build/static/media/wallpaperDefaut.adbccee0.png"
+  }
+]);
